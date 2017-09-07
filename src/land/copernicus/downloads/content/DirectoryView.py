753681a290cacd93b8ce55d498118423d4257eee
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2017-09-07 20:41:20 +0300

    Override index_html

--- src/land/copernicus/downloads/content/DirectoryView.py
+++ src/land/copernicus/downloads/content/DirectoryView.py
@@ -6,10 +6,15 @@ from eea.downloads.content.DirectoryView import PatchedFSFile as FSFile
 class PatchedFSFile(FSFile):
     """ Custom FS File
     """
-    def manage_FTPget(self, REQUEST, RESPONSE):
-        """ Custom ftp """
+    # def manage_FTPget(self, REQUEST, RESPONSE):
+    def index_html(self, REQUEST, RESPONSE):
+        """ Custom http / ftp get """
+
         print "=== Google analytics here ==="
-        return super(PatchedFSFile, self).manage_FTPget(REQUEST, RESPONSE)
+
+        # return super(PatchedFSFile, self).manage_FTPget(REQUEST, RESPONSE)
+        return super(PatchedFSFile, self).index_html(REQUEST, RESPONSE)
+
 
 DirectoryView.registerFileExtension('zip', PatchedFSFile)
 
